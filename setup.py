61f04687733f84040e200c141ddff6d6c924b54b
almemanuel <emanuel.almeida1998@outlook.com>
2024-04-14 16:49:19 -0300

    try a package

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='sharepoint-lib',
+    name='pypoint',
     version='0.1',
     description='Library for interacting with SharePoint',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Emanuel Almeida',
     author_email='emanuel.almeida1998@outlook.com',
-    url='https://github.com/almemanuel/pypoint/pypoint',
+    url='https://github.com/almemanuel/pypoint',
     packages=find_packages(),
     install_requires=[
         'office365-rest-python-client == 2.2.0'
